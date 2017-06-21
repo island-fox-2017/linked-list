ac8611263ee36f3f51a2c9ecb4383f318e1076ca
name": null <email": null>
2017-06-21 20:56:01 +0700

    linked list array & linked list queue DONE

--- linked_list_queue.js
+++ linked_list_queue.js
@@ -1,4 +1,42 @@
 //write your code here
+class LinkedList {
+  constructor (arr) {
+    this.arr = arr;
+    this.linkedList = [];
+    for (let i = 0; i < this.arr.length; i++) {
+      this.linkedList.push([]);
+      this.linkedList[i][0] = this.arr[i];
+      this.linkedList[i][1] = i + 1;
+    }
+    this.linkedList[this.arr.length - 1][1] = null;
+  }
+  
+  printAll () {
+    return this;
+  }
+  
+  getLength () {
+    return this.linkedList.length;
+  }
+  
+  getHead () {
+    return this.linkedList[0][0];
+  }
+  
+  getTail() {
+    return this.linkedList[this.linkedList.length - 1][0];
+  }
+  
+  push(x) {
+    this.linkedList[this.linkedList.length - 1][1] = this.linkedList.length;
+    this.linkedList.push([x, null]);
+  }
+  
+  pop() {
+    this.linkedList.pop();
+    this.linkedList[this.linkedList.length - 1][1] = null;
+  }
+}
 
 let list = new LinkedList([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);
 console.log(list.getLength());
@@ -6,3 +44,5 @@ console.log(list.getHead());
 console.log(list.getTail());
 list.push(11);
 list.pop();
+console.log(list.getTail());
+console.log(list.printAll());
